0832ecd8c0b2a94533158dc92a591d3e2a732712
Maxim <sulky.cat.maxim@gmail.com>
2024-07-19 17:36:38 +0300

    Add img

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,12 +1,12 @@
-import EmptyImg from '../../dist/assets/Empty.png'
+import EmptyImg from "../../public/Empty.png";
 
 export default function Empty() {
-   return (
-      <div className="empty">
-         <div className="empty__img">
-            <img src={EmptyImg} alt="Empty" />
-         </div>
-         <p className='empty__text'>Empty...</p>
+  return (
+    <div className="empty">
+      <div className="empty__img">
+        <img src={EmptyImg} alt="Empty" />
       </div>
-   )
+      <p className="empty__text">Empty...</p>
+    </div>
+  );
 }
